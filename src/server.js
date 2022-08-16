5102c6c504bc52cfce3750d36b954c712c3bdc34
Francisco Morales Sánchez <fcomorales.sanchez@gmail.com>
2022-08-16 02:56:05 -0400

    Create todos method

--- src/server.js
+++ src/server.js
@@ -1,10 +1,37 @@
 const express = require('express');
+const uuid = require('uuid');
+
+let fakeTodos = [
+    {
+        id: '123',
+        text: 'Go to the grocery store',
+        isCompleted: false
+    },
+    {
+        id: '234',
+        text: 'Learn full stack',
+        isCompleted: true
+    }
+];
 
 const app = express();
+app.use(express.json());
 
-app.get('/test', (req, res) => {
-    res.send('Hello from the backend')
-})
+app.get('/todos', (req, res) => {
+    res.json(fakeTodos);
+});
+
+app.post('/todos', (req, res) => {
+    const newTodoText = req.body.newTodoText;
+    const newTodo = {
+        id: uuid.v4(),
+        text: newTodoText,
+        isCompleted: false
+    }
+    fakeTodos.push(newTodo);
+    res.json(newTodo);
+});
+
 
 app.listen(8088, () => {
     console.log('Server is listening on port 8088');
